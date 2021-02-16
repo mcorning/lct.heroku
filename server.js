29a41a3d3a78cf7304a6ae120ee570bc88e9872c
Michael Corning <michael@secours.io>
2021-02-15 17:07:04 -0800

    added lct-b

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
 const http = require('http').createServer(app);
 let version = '0.4';
+
+app.use(express.static(path.join(__dirname, './dist')));
 
 http.listen(process.env.port, () => {
   console.log(`Server.js Build: ${version}`);
