7f0be8c4556597b8d794b9b041d4d9ab8c9fd86c
Michael Corning <michael@secours.io>
2021-02-15 16:49:46 -0800

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const http = require('http').createServer(app);
+let version = '0.4';
+
+http.listen(process.env.port, () => {
+  console.log(`Server.js Build: ${version}`);
+  console.log(`Server running on http://localhost:${process.env.port}`);
+  console.log(' ');
+});
